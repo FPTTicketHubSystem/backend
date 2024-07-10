bd5912a0a4bac6c7825c679679da5f6299431ace
Ace <qanhltv11@gmail.com>
2024-07-10 10:25:17 +0700

    Update Manage News

--- backend/DTOs/NewsDTO.cs
+++ backend/DTOs/NewsDTO.cs
@@ -18,4 +18,36 @@
 
         public string Status { get; set; }
     }
+    public class DailyNews
+    {
+        public int? NewsId { get; set; }
+        public string? CategoryName { get; set; }
+        public string? Title { get; set; }
+        public string? Image { get; set; }
+    }
+    public class NewDetail
+    {
+        public int? NewsId { get; set; }
+        public string? Title { get; set; }
+        public string? Subtitle { get; set; }
+        public string? Content { get; set; }
+        public string? CreateDate { get; set; }
+    }
+    public class FirstNews
+    {
+        public int? NewsId { get; set; }
+        public string? Title { get; set; }
+        public string? Image { get; set; }
+        public string? SubTitle { get; set; }
+        public string? Content { get; set; }
+        public string? CreatedDay { get; set; }
+    }
+    public class OtherNews
+    {
+        public int? NewsId { get; set; }
+        public string? Title { get; set; }
+        public string? Image { get; set; }
+        public string? SubTitle { get; set; }
+    }
 }
+
